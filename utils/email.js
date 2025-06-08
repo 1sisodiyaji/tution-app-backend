d030c9f767100e80d9626f40e4394d1882e873e7
Golu_singh <637golusingh@gmail.com>
2025-06-08 15:55:23 +0530

    Assets done

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ const getEmailTemplate = require('../config/GetEmailTemplate');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: constants.SMTP_PORT === '465',
+  secure: process.env.SMTP_PORT === '465',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
