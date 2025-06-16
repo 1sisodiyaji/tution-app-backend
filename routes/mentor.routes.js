a1073babae25718d2e7821d363d60251fb2892a0
Golu_singh <637golusingh@gmail.com>
2025-06-16 23:49:20 +0530

    msg

--- routes/mentor.routes.js
+++ routes/mentor.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const teacherController = require('../controllers/teacherController');
+const teacherController = require('../controllers/teacher.controller');
 
 router.get('/all-teachers', teacherController.GetAllMentors);
 router.get('/profile/:username', teacherController.GetMentorByUserName);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/userController');
+const userController = require('../controllers/user.controller');
 const { validateRegister, validateLogin, validforgotPassword } = require('../middleware/validator');
 const protect = require('../middleware/auth');
 const {
